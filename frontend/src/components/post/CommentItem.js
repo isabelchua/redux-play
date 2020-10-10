150b50f77fb67217be35f2c96b8f2ff5f84531f8
isabelchua <isabelchua86@gmail.com>
2020-10-10 14:12:23 -0700

    review layout changes

--- frontend/src/components/post/CommentItem.js
+++ frontend/src/components/post/CommentItem.js
@@ -9,55 +9,72 @@ const CommentItem = ({
 	addLike,
 	removeLike,
 	postId,
-	comment: { _id, text, name, avatar, user, likes, date, rating },
+	comment: { _id, text, name, avatar, user, likes, date, rating, location },
 	auth,
 	deleteComment
 }) => {
 	return (
 		<div className="post">
 			<div className="user-profile-post">
-				<Link to={`/profile/${user}`}>
-					<img className="round-img avatar" src={avatar} alt="" />
-					<div className="user-details-post">
-						<p>{name}</p>
+				<div className="review-top">
+					<Link to={`/profile/${user}`}>
+						<img className="round-img avatar" src={avatar} alt="" />
+						<div className="user-details-post">
+							<strong>
+								<p>{name}</p>
+							</strong>
+							{/* <p>{location}</p> */}
+							<p>San Francisco</p>
+						</div>
+					</Link>
+
+					<div className="review-upper-right">
+						<Rating name="read-only" value={5} readOnly precision={0.5} />
+						<p className="post-date">
+							Reviewed on <Moment format="MM/DD/YYYY">{date}</Moment>
+						</p>
 					</div>
-				</Link>
+				</div>
 			</div>
 			<div className="post-content">
-				<Rating name="read-only" value={5} readOnly precision={0.5} />
-				<p className="post-date">
-					Posted on <Moment format="MM/DD/YYYY">{date}</Moment>
-				</p>
 				<p className="my-1">{text}</p>
-				<span>{likes.length > 0 && <span>{likes.length} people found this review helpful</span>} </span>
 
-				<div>was this review helpful?
-				<button
-					onClick={e => addLike(postId, _id)}
-					type="button"
-					className="btn thumb green"
-				>
-					<i className="fas fa-thumbs-up" />{" "}
-					
-				</button>
-				<button
-					onClick={e => removeLike(postId, _id)}
-					type="button"
-					className="btn thumb red"
-				>
-					<i className="fas fa-thumbs-down" />
-				</button>
-	</div>
-				{auth.user && !auth.loading && user === auth.user._id && (
-					<button
-						onClick={e => deleteComment(postId, _id)}
-						type="button"
-						className="btn thumb delete"
-					>
-						<i className="fas fa-times"></i>
-					</button>
-				)}
+				<div className="review-bottom">
+					<div className="review-bottom-left">
+						{likes.length > 0 && (
+							<div>{likes.length} people found this review helpful</div>
+						)}{" "}
+					</div>
+					<div className="review-bottom-right">
+						<span className="space-right">Was this review helpful?</span>
+
+						<button
+							onClick={e => addLike(postId, _id)}
+							type="button"
+							className="btn thumb green"
+						>
+							<i className="fas fa-thumbs-up" />{" "}
+						</button>
+						<button
+							onClick={e => removeLike(postId, _id)}
+							type="button"
+							className="btn thumb red"
+						>
+							<i className="fas fa-thumbs-down" />
+						</button>
+					</div>
+				</div>
 			</div>
+			{auth.user && !auth.loading && user === auth.user._id && (
+				<button
+					onClick={e => deleteComment(postId, _id)}
+					type="button"
+					className="btn thumb delete"
+				>
+					<span className="space-right">Delete this Review</span>{" "}
+					<i className="fas fa-times" />
+				</button>
+			)}
 		</div>
 	);
 };
